5c5824e993ec1e835ef91f7585a2fe8ecf027e90
leonardo <leo-feitosa@live.com>
2025-03-28 19:35:40 -0300

    chore: config federation

--- packages/host/vite.config.ts
+++ packages/host/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, ViteDevServer } from 'vite';
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react';
+import { IncomingMessage } from 'http';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'app',
+      remotes: {
+        remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+    {
+      name: 'vite-plugin-reload-endpoint',
+      configureServer(server: ViteDevServer) {
+        server.middlewares.use((req:IncomingMessage, res, next) => {
+          if (req.url === '/__fullReload') {
+            server.hot.send({ type: 'full-reload' });
+
+            res.end('Full reload triggered');
+          } else {
+            next();
+          }
+        });
+      },
+    },
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
